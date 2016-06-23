8ac7c8010912fb522a71e8043a6d2b0450d08115
Abel <kent62001@gmail.com>
2016-06-23 15:55:11 +0800

    [追蹤] - 修復ga

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -203,12 +203,12 @@
 @yield('footer')
 <!-- ga code -->
 <script>
-    (function(i,s,o,g,r,a,m){i['googleanalyticsobject']=r;i[r]=i[r]||function(){
-                (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new date();a=s.createelement(o),
-            m=s.getelementsbytagname(o)[0];a.async=1;a.src=g;m.parentnode.insertbefore(a,m)
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+                (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
     })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
-    ga('create', 'ua-78061291-1', 'auto');
+    ga('create', 'UA-78061291-1', 'auto');
     ga('send', 'pageview');
 
 </script>
