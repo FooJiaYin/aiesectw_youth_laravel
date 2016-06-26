4078708b4c2d6e7f8f826ebfd634fd7ada58a08c
Abel <kent62001@gmail.com>
2016-06-26 17:42:12 +0800

    [JS] - 修改控制台使用的圖表js

--- public/auth/pages/morris.init.js
+++ public/auth/pages/morris.init.js
@@ -11,7 +11,7 @@
     var MorrisCharts = function() {};
 
     //creates line chart
-    MorrisCharts.prototype.createLineChart = function(element, data, xkey, ykeys, labels, opacity, Pfillcolor, Pstockcolor, lineColors) {
+    MorrisCharts.prototype.createLineChart = function(element, data, xkey, ykeys, labels, opacity, Pfillcolor, Pstockcolor, lineColors, time) {
         Morris.Line({
           element: element,
           data: data,
@@ -25,15 +25,16 @@
           gridLineColor: '#eef0f2',
           hideHover: 'auto',
           resize: true, //defaulted to true
+          parseTime: time,
           lineColors: lineColors
         });
     },
     //creates area chart
-    MorrisCharts.prototype.createAreaChart = function(element, pointSize, lineWidth, data, xkey, ykeys, labels, lineColors) {
+    MorrisCharts.prototype.createAreaChart = function(element, pointSize, lineWidth, data, xkey, ykeys, labels, lineColors, time) {
         Morris.Area({
             element: element,
             pointSize: 0,
-            lineWidth: 0,
+            lineWidth: 1,
             data: data,
             xkey: xkey,
             ykeys: ykeys,
@@ -41,6 +42,7 @@
             hideHover: 'auto',
             resize: true,
             gridLineColor: '#eef0f2',
+            parseTime: time,
             lineColors: lineColors
         });
     },
@@ -103,15 +105,11 @@
     MorrisCharts.prototype.init = function() {
 
         //create line chart
-        var $data  = [
-            { y: '2010', a: 30,  b: 20 , c: 10 },
-            { y: '2011', a: 50,  b: 40 , c: 30 },
-            { y: '2012', a: 75,  b: 65 , c: 50 },
-            { y: '2013', a: 50,  b: 40 , c: 22 },
-            { y: '2014', a: 75,  b: 65 , c: 50 },
-            { y: '2015', a: 100, b: 90 , c: 65 }
-          ];
-        this.createLineChart('morris-line-example', $data, 'y', ['a', 'b','c'], ['Series A', 'Series B', 'Series C'],['0.1'],['#ffffff'],['#999999'], ["#f9cd48", "#29b6f6", "#3f51b5"]);
+        var $data = $('#morris-weekly-page-view').data('chart');
+        this.createLineChart('morris-weekly-page-view', $data, 'week', ['lastPageViews', 'thisPageViews'], ['上週','本週'],['0.1'],['#ffffff'],['#999999'], ["#f9cd48", "#29b6f6"], false);
+
+        var $data = $('#morris-weekly-visitors').data('chart');
+        this.createLineChart('morris-weekly-visitors', $data, 'week', ['lastVisitors', 'thisVisitors'], ['上週','本週'],['0.1'],['#ffffff'],['#999999'], ["#f9cd48", "#29b6f6"], false);
 
         //creating area chart
         var $areaData = [
@@ -123,19 +121,20 @@
                 { y: '2014', a: 75, b: 65, c:30 },
                 { y: '2015', a: 90, b: 60, c:30 }
             ];
-        this.createAreaChart('morris-area-example', 0, 0, $areaData, 'y', ['a', 'b','c'], ['Series A', 'Series B','Series C'], ["#f9cd48", "#64b5f6","#dcdcdc"]);
+        var $areaData = $('#morris-hourly-actions').data('chart');
+        this.createAreaChart('morris-hourly-actions', 0, 0, $areaData, 'hour', ['yesterday', 'today'], ['昨天', '今天'], ["#f9cd48", "#64b5f6","#dcdcdc"], false);
 
         //creating area chart with dotted
-        var $areaDotData = [
-                { y: '2009', a: 10, b: 20 },
-                { y: '2010', a: 75,  b: 65 },
-                { y: '2011', a: 50,  b: 40 },
-                { y: '2012', a: 75,  b: 65 },
-                { y: '2013', a: 50,  b: 40 },
-                { y: '2014', a: 75,  b: 65 },
-                { y: '2015', a: 90, b: 60 }
-            ];
-        this.createAreaChartDotted('morris-area-with-dotted', 0, 0, $areaDotData, 'y', ['a', 'b'], ['Series A', 'Series B'],['#ffffff'],['#999999'], ["#f9cd48", "#64b5f6"]);
+        //var $areaDotData = [
+        //        { y: '2009', a: 10, b: 20 },
+        //        { y: '2010', a: 75,  b: 65 },
+        //        { y: '2011', a: 50,  b: 40 },
+        //        { y: '2012', a: 75,  b: 65 },
+        //        { y: '2013', a: 50,  b: 40 },
+        //        { y: '2014', a: 75,  b: 65 },
+        //        { y: '2015', a: 90, b: 60 }
+        //    ];
+        //this.createAreaChartDotted('morris-area-with-dotted', 0, 0, $areaDotData, 'y', ['a', 'b'], ['Series A', 'Series B'],['#ffffff'],['#999999'], ["#f9cd48", "#64b5f6"]);
 
         //creating bar chart
         var $barData  = [
@@ -147,7 +146,7 @@
             { y: '2014', a: 75,  b: 65 , c: 56 },
             { y: '2015', a: 100, b: 90 , c: 60 }
         ];
-        this.createBarChart('morris-bar-example', $barData, 'y', ['a', 'b', 'c'], ['Series A', 'Series B', 'Series C'], ["#f9cd48", "#64b5f6","#dcdcdc"]);
+        //this.createBarChart('morris-bar-example', $barData, 'y', ['a', 'b', 'c'], ['Series A', 'Series B', 'Series C'], ["#f9cd48", "#64b5f6","#dcdcdc"]);
 
         //creating Stacked chart
         var $stckedData  = [
@@ -163,7 +162,7 @@
             { y: '2014', a: 75,  b: 65 },
             { y: '2015', a: 100, b: 90 }
         ];
-        this.createStackedChart('morris-bar-stacked', $stckedData, 'y', ['a', 'b'], ['Series A', 'Series B'], ["#f9cd48", "#ededed"]);
+        //this.createStackedChart('morris-bar-stacked', $stckedData, 'y', ['a', 'b'], ['Series A', 'Series B'], ["#f9cd48", "#ededed"]);
 
         //creating donut chart
         var $donutData = [
@@ -171,7 +170,8 @@
                 {label: "In-Store Sales", value: 30},
                 {label: "Mail-Order Sales", value: 20}
             ];
-        this.createDonutChart('morris-donut-example', $donutData, ["#f9cd48", "#ededed", "#64b5f6"]);
+        var $donutData = $('#morris-monthly-devices').data('chart');
+        this.createDonutChart('morris-monthly-devices', $donutData, ["#f9cd48", "#ededed", "#64b5f6"]);
     },
     //init
     $.MorrisCharts = new MorrisCharts, $.MorrisCharts.Constructor = MorrisCharts
