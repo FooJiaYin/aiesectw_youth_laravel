fcd6eb3a748ebeccd7f4b45fe419edf263d4261e
Abel <kent62001@gmail.com>
2016-07-09 22:11:25 +0800

    [Auth] - 修改Auth model位置

--- config/auth.php
+++ config/auth.php
@@ -67,7 +67,7 @@ return [
     'providers' => [
         'users' => [
             'driver' => 'eloquent',
-            'model' => App\User::class,
+            'model' => App\YouthSpeak\User\Entities\User::class,
         ],
 
         // 'users' => [
