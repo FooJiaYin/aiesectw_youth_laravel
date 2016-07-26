51d38919edd82ed36b96556d8ff4ddd4dd16d305
Abel <kent62001@gmail.com>
2016-07-26 19:51:06 +0800

    [多國語言] - 更新媒體庫

---
+++ resources/lang/zh_TW/photo.php
@@ -0,0 +1,12 @@
+<?php
+return [
+    'photo'                 => '媒體庫',
+    /**
+     * Action
+     */
+    'upload-file'           => '上傳檔案',
+    'upload-file-detail'    => '支援多檔上傳，同時上傳20個檔案，每個檔案不超過2MB',
+    'upload-front-image'    => '上傳封面照片',
+    'select-front-image'    => '選擇封面照片',
+
+];
